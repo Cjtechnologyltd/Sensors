75b53a87db5bb9923fd0c3abd556387eeb905054
Cjtechnologyltd <113897859+Cjtechnologyltd@users.noreply.github.com>
2023-01-25 11:01:45 +0000

    starting from a fresh example project provided by VS

---
+++ Sensors/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Sensors;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
