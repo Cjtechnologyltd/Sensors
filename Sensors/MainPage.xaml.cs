5c3b7c79ae149e71477c5f36d14372336257e483
Cjtechnologyltd <113897859+Cjtechnologyltd@users.noreply.github.com>
2025-02-01 01:28:26 +0000

    Add gyro and magnetometer

--- Sensors/MainPage.xaml.cs
+++ Sensors/MainPage.xaml.cs
@@ -49,5 +49,55 @@ public partial class MainPage : ContentPage
 	{
         AccelLabel.Text = $"Accel: {e.Reading}";
     }
+
+    private void ToggleGyroscope(object sender, EventArgs e)
+    {
+        if (Gyroscope.Default.IsSupported)
+        {
+            if (!Gyroscope.Default.IsMonitoring)
+            {
+                // Turn on gyroscope
+                Gyroscope.Default.ReadingChanged += Gyroscope_ReadingChanged;
+                Gyroscope.Default.Start(SensorSpeed.UI);
+            }
+            else
+            {
+                // Turn off gyroscope
+                Gyroscope.Default.Stop();
+                Gyroscope.Default.ReadingChanged -= Gyroscope_ReadingChanged;
+            }
+        }
+    }
+    private void Gyroscope_ReadingChanged(object sender, GyroscopeChangedEventArgs e)
+    {
+        // Update UI Label with gyroscope state
+        GyroscopeLabel.TextColor = Colors.Green;
+        GyroscopeLabel.Text = $"Gyroscope: {e.Reading}";
+    }
+
+    private void ToggleMagnetometer(object sender, EventArgs e)
+    {
+        if (Magnetometer.Default.IsSupported)
+        {
+            if (!Magnetometer.Default.IsMonitoring)
+            {
+                // Turn on magnetometer
+                Magnetometer.Default.ReadingChanged += Magnetometer_ReadingChanged;
+                Magnetometer.Default.Start(SensorSpeed.UI);
+            }
+            else
+            {
+                // Turn off magnetometer
+                Magnetometer.Default.Stop();
+                Magnetometer.Default.ReadingChanged -= Magnetometer_ReadingChanged;
+            }
+        }
+    }
+    private void Magnetometer_ReadingChanged(object sender, MagnetometerChangedEventArgs e)
+    {
+        // Update UI Label with magnetometer state
+        MagnetometerLabel.TextColor = Colors.Green;
+        MagnetometerLabel.Text = $"Magnetometer: {e.Reading}";
+    }
 }
 
